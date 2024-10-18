e9a4451944c5de341509e6e236ad23aca664518a
Partybox98 <kirbymmo@gmail.com>
2024-10-18 14:43:44 -0700

    Sword work :) :100:

--- Chance class game jam/Assets/Scripts/attack.cs
+++ Chance class game jam/Assets/Scripts/attack.cs
@@ -6,13 +6,15 @@ using UnityEngine.UIElements;
 public class attack : MonoBehaviour
 {
     private BoxCollider hitbox;
-    private MeshRenderer renderer;
+    private MeshRenderer render;
 
     // Start is called before the first frame update
     void Start()
     {
-        hitbox = GetComponent<BoxCollider>();
-        renderer = GetComponentInChildren<MeshRenderer>();
+        hitbox = GetComponentInChildren<BoxCollider>();
+        render = GetComponentInChildren<MeshRenderer>();
+        hitbox.enabled = false;
+        
         transform.Rotate(0, -55.0f, 0, Space.Self);
     }
 
@@ -26,13 +28,15 @@ public class attack : MonoBehaviour
     }
     IEnumerator Rotate()
     {
-        renderer.enabled = true;
+        render.enabled = true;
+        hitbox.enabled = true;
         for (int i = 0; i <22; i++)
         {
             transform.Rotate(0, 5.0f, 0, Space.Self);
             yield return new WaitForSeconds(.01f);
         }
-        renderer.enabled = false;
+        render.enabled = false;
+        hitbox.enabled = false;
         transform.Rotate(0, -110.0f, 0, Space.Self);
     }
 }
--- Chance class game jam/Assets/Scripts/enemyAi.cs
+++ Chance class game jam/Assets/Scripts/enemyAi.cs
@@ -1,23 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class enemyAi : MonoBehaviour
 {
+    public int maxHealth = 5;
+    private int health;
     public GameObject destination;
     public NavMeshAgent agent;
+    public Rigidbody body;
+    public CapsuleCollider capsule;
+    public string weaponTag;
     // public Bullet bulletPrefab;
     
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Running");
         agent = GetComponent<NavMeshAgent>();
+        body = GetComponent<Rigidbody>();
+        capsule = GetComponent<CapsuleCollider>();
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.destination = destination.transform.position;
+    }
+
+    void OnTriggerEnter(Collider collision) {
+        Debug.Log("trigger entered");
+        if (collision.gameObject.tag == weaponTag) {
+            onHit();
+        }
+    }
+
+    void onHit(){
+        Debug.Log("enemy hit");
+        health -= 1;
+        if (health <= 0) {
+            die();
+        }
+    }
+    void onHit(int damage){
+        health -= damage;
+        if (health <= 0) {
+            die();
+        }
+    }
+    void die() {
+        Debug.Log("enemy died");
+        gameObject.SetActive(false);
     }
 }
