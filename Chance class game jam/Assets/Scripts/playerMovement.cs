b5b3ce7014e8a93eaca14bfd423ce77392ed3e06
Partybox98 <kirbymmo@gmail.com>
2024-10-21 15:25:16 -0700

    added health bar sprites for visual health

--- Chance class game jam/Assets/Scripts/playerMovement.cs
+++ Chance class game jam/Assets/Scripts/playerMovement.cs
@@ -31,4 +31,22 @@ public class Playermovement : MonoBehaviour
 			Mathf.Lerp(0, Input.GetAxis("Vertical")* curSpeed, 0.8f)
 		);
     }
+
+    void onHit(){
+        Debug.Log("player hit");
+        health -= 1;
+        if (health <= 0) {
+            die();
+        }
+    }
+    void onHit(int damage){
+        Debug.Log("player hit");
+        health -= damage;
+        if (health <= 0) {
+            die();
+        }
+    }
+    void die() {
+        Debug.Log("player died :(");
+    }
 }
